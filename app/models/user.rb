47779db54ecbe4cc73dc669a5f0093239882c844
Aaron Na <syn228@nyu.edu>
2018-08-02 09:28:40 -0400

    seed clear and username/password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
 
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :username, presence: true, uniqueness: true
-    validates :password, presence: true
+    validates :username, presence: true, uniqueness: true, length: {minimum: 4}
+    validates :password, presence: true, length: {minimum: 8}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-aaron = User.create(username: "aarongy", password:"a", first_name:"aaron", last_name:"na")
-
-file1 = Convertedfile.create(name: "my doc!", user_id: User.first.id, size: 500, text: "WOW")
-file2 = Convertedfile.create(name: "my doc2!", user_id: User.first.id, size: 50000, text: "this is a longer example that I would like to test with moving forward")
-
-upload1 = Upload.create(name: "filename", path: "/good/path")
+rspect = User.create(username: "rspect", password:"123", first_name:"put", last_name:"rspect")
